c858052a04086139e9d7f3299608fd22d02a7469
nhatnguyen <nguyenphn1441@ut.edu.vn>
2025-05-23 19:44:04 +0700

    nhatnguyen commit

--- singleLinkedList2.cpp
+++ singleLinkedList2.cpp
@@ -16,25 +16,22 @@ public:
 class SingleLinkedList {
 private:
   Node *head;
-  int count;
 
 public:
   // Constructor: Trùng tên với class
   SingleLinkedList() {
     head = nullptr;
-    count = 1;
   }
   // Destructor: Xoá giá trị
   ~SingleLinkedList() {}
   void insertNode(Node *node, int index) {
-    if (index >= count || index < 0) {
+    if (index >= countNode() || index < 0) {
       cout << "Vị trí thêm không hợp lệ!" << endl;
       return;
     }
     if (index == 0) {
       node->next = head;
       head = node;
-      count++;
       return;
     }
     Node *current = head;
@@ -43,15 +40,79 @@ public:
     }
     node->next = current->next;
     current->next = node;
-count++;
   }
+  bool deleteNode(int val){
+    if (head == nullptr){
+      return false;
+    }
+    // delete head
+    if(head->value == val){
+      Node* temp = head;
+      head = head->next;
+      delete temp;
+      return true;
+    }
+    Node* temp = head;
+    while(temp->next != nullptr && temp->next->value != val){
+      temp = temp->next;
+    }
+    if(temp->next == nullptr){
+      return false;
+    }
+    Node* nodeToDelete = temp->next;
+    temp->next = temp->next->next;
+    delete nodeToDelete;
+    return true;
+    //temp = v1
+    // v1 v2 v3 v4
+  }
+  bool search(int val){
+    Node* current = head;
+    while(current != nullptr){
+      if(current->value == val){
+        return true;
+      }
+      current = current->next;
+
+    }
+    return false;
+  }
+  int countNode(){
+    int count = 0;
+    Node* current = head;
+    while(current != nullptr){
+      count++;
+      current = current->next;
+    }
+    return count;
+  }
+  void deleteAll(){
+    Node* current = head;
+    while(current != nullptr){
+      Node* temp = current;
+      current = current->next;
+      delete temp;
+    }
+    head = nullptr;
+  }
+  int sum(){
+    int sum = 0;
+    Node* current = head;
+    while(current != nullptr){
+      sum += current->value;
+      current = current->next;
+
+    }
+    return sum;
+  }
+
   void print(){
     if(!head)
     {
         return;
     }
     Node* current = head;
-    for(int i = 0; i < count - 1; i++){
+    for(int i = 0; i < countNode() - 1; i++){
         cout << current->value << " ";
         current = current->next;
 
@@ -72,5 +133,14 @@ int main()
     Node* n3 = new Node(4);
     list->insertNode(n3, 0);
     list->print();
+    list->deleteNode(4);
+    cout << "Sau khi xóa số 4: " << endl;
+    list->print();
+    cout << "Tổng: " << list->sum() << endl;
+    cout << "Số lượng node: " << list->countNode() << endl;
+    list->deleteAll();
+    list->print();
+
+
 
 }
